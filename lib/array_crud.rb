2345870ca6a9397113ed8e1fc0d1076e15a9212a
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-27 03:33:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = [1, 2, 3, 4, "arrays!"]
-  "arrays!" = array.shift
+  "arrays!" = array.pop
 end
 
 def remove_element_from_start_of_array(array)
