c3b728c1645a3ab5e84efc9f5d912b65f1f2d043
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-27 03:21:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1, 2, 3, 4]
+  element = "arrays!"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
+  array = [1, 2, 3, 4]
+  element = "wow"
+  element = add_element_to_start_of_array.shift
   
 end
 
