4428861fbc08d995131b1faa369e04772a27daf5
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-27 03:44:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", 2, 3, 4, "am"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
